35eb0403d0a046af4998792f06f756708d40deef
Nicolas Gimenez <nico.787@hotmail.com>
2016-11-02 21:45:59 -0300

    first

---
+++ src/fiubaceldas/grupo03/Agent.java
@@ -0,0 +1,22 @@
+package fiubaceldas.grupo03;
+
+import core.game.StateObservationMulti;
+import core.player.AbstractMultiPlayer;
+import juanma.Perception;
+import ontology.Types.ACTIONS;
+import tools.ElapsedCpuTimer;
+
+public class Agent extends AbstractMultiPlayer {
+
+	public Agent(StateObservationMulti stateObs, ElapsedCpuTimer elapsedTimer, int playerID) {
+	}
+	
+	@Override
+	public ACTIONS act(StateObservationMulti stateObs, ElapsedCpuTimer elapsedTimer) {
+		Perception perception = new Perception(stateObs);
+        System.out.println(perception.toString());
+
+		return ACTIONS.ACTION_NIL;
+	}
+
+}
